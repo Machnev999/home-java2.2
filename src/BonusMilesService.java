15d88749fd8e01c72ef05904023f3e987d3627f6
Kir <87722197+Machnev999@users.noreply.github.com>
2021-09-10 16:32:06 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
     public int calculate(int price) {
         int milePrice = 20;
-        int countMile = price / milePrice;
-        return countMile;
+        return price / milePrice;
     }
 }
